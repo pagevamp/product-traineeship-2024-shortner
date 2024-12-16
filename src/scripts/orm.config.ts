df1a36b13c569b72b6016ff453f4a10b4c5a77bc
Pratham-outside <pratham.sapkota@outside.tech>
2024-12-16 16:35:49 +0545

    feat(database): integrated database and its configurations for postgres

---
+++ src/scripts/orm.config.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+import { join } from 'path';
+config();
+
+export const dataBaseConfigurations = {
+	type: 'postgres',
+	port: +(process.env.POSTGRES_PORT || '5432'),
+	username: process.env.POSTGRES_USER,
+	password: process.env.POSTGRES_PASSWORD,
+	database: process.env.POSTGRES_DB,
+	synchronize: false, // Should be false in production to use migrations
+	logging: true,
+	entities: [join(__dirname, '/../entities', '*.entity.{ts,js}')],
+	migrations: [join('__dirname', '/../migrations', '*.{ts,js}')],
+};
