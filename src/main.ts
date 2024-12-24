fcc07df85ddf87695fbacb9584fcc493b34ea1cf
Narayan Sapkota <sapkota.king@gmail.com>
2024-12-24 11:30:15 +0545

    feat: add logging service with winston

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import { loggerConfig } from '@/config/logger.config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
-	const app = await NestFactory.create(AppModule);
-
-	await app.listen(process.env.APP_PORT ?? 3000, () => {
-		console.info('Listening to server....');
-		console.info(`Server listening at port http://localhost:${process.env.APP_PORT}`);
+	const logger = new Logger();
+	const port = process.env.APP_PORT ?? 3000;
+	const app = await NestFactory.create(AppModule, {
+		// this will overwrite the default logger of nestJS with custom winston logger
+		logger: WinstonModule.createLogger(loggerConfig),
+	});
+	await app.listen(port, () => {
+		logger.log(`App is listening on port ${port}`);
 	});
 }
 
