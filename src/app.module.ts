1a3a1944acc0edf2e416182439ac99a97a4c0809
pratham-outside <pratham.sapkota@outside.tech>
2025-01-02 17:38:30 +0545

    chore: changed env.example.ts,modified docker file and added common message response and messages

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { validate } from '@/config/env.config';
 import { DatabaseModule } from '@/database/db.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true, validate }), DatabaseModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true, validate }), DatabaseModule, UsersModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
---
+++ src/common/messages.ts
@@ -0,0 +1,4 @@
+export const success_message = {
+	user_created: 'User has been created successfully',
+	user_fetched: 'User/s has been fetched successgully',
+};
