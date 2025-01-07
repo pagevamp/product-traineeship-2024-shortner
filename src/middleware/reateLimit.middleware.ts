fed1a60ee3ea060ea77f899580d5e19309f018d1
Narayan Sapkota <59175636+noxiousghost@users.noreply.github.com>
2025-01-07 21:34:32 +0545

    PDT24-40 | Setup Rate limiter service (#14)
    * chore(redis): setup redis in the project
    
    * chore(rate limiter): configure rate limiter middleware
    
    * test: rate limit application example
    
    * chore: make values in interface compulsory
    
    * chore: add dummy values to few env variables

---
+++ src/middleware/reateLimit.middleware.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import rateLimit from 'express-rate-limit';
+import { RedisStore } from 'rate-limit-redis';
+import { redisClient } from '@/config/redis.config';
+
+interface RateLimitConfig {
+	windowMs: number;
+	max: number;
+}
+
+@Injectable()
+export class RateLimitMiddlewareFactory {
+	static create(config: RateLimitConfig): new () => NestMiddleware {
+		@Injectable()
+		class CustomRateLimit implements NestMiddleware {
+			private limiter = rateLimit({
+				windowMs: config.windowMs || 15 * 60 * 1000,
+				max: config.max || 100,
+				message: {
+					status: 429,
+					error: 'Too many requests',
+					message: 'Too many requests from this IP, please try again later.',
+				},
+				standardHeaders: true,
+				legacyHeaders: false,
+				store: new RedisStore({
+					sendCommand: (...args: string[]) => redisClient.sendCommand(args),
+				}),
+			});
+
+			use(req: Request, res: Response, next: NextFunction) {
+				this.limiter(req, res, next);
+			}
+		}
+		return CustomRateLimit;
+	}
+}
