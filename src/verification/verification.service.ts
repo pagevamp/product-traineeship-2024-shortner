7ab04da04cb42f6a010648cece55b2c4dc9f0842
Narayan Sapkota <sapkota.king@gmail.com>
2025-01-07 08:07:41 +0545

    refactor: provide meaningful name to few variables

--- src/verification/verification.service.ts
+++ src/verification/verification.service.ts
@@ -15,26 +15,26 @@ export class VerificationService {
 	) {}
 
 	async createOtp(user_id: string, size = 6): Promise<string> {
-		const recentToken = await this.otpRepository.findOne({
+		const recentOtpRequest = await this.otpRepository.findOne({
 			where: {
 				user_id,
 				created_at: MoreThan(new Date(new Date().getTime() - this.minutesToMakeNewOtpRequest * 60 * 1000)),
 			},
 		});
-		if (recentToken) {
+		if (recentOtpRequest) {
 			throw new UnprocessableEntityException(
 				`Please wait ${this.minutesToMakeNewOtpRequest} minutes to make new request.`,
 			);
 		}
 		const otp = generateOtp(size);
-		const hashedCode = await hash(otp, 10);
-		const otpEntity = this.otpRepository.create({
+		const hashedOtp = await hash(otp, 10);
+		const saveOtp = this.otpRepository.create({
 			user_id,
-			otp_code: hashedCode,
+			otp_code: hashedOtp,
 			expires_at: new Date(new Date().getTime() + this.otpExpirationMinutes * 60 * 1000),
 		});
 		await this.otpRepository.delete({ user_id });
-		await this.otpRepository.save(otpEntity);
+		await this.otpRepository.save(saveOtp);
 		return otp;
 	}
 
@@ -43,7 +43,7 @@ export class VerificationService {
 			where: { user_id, expires_at: MoreThan(new Date()) },
 		});
 		if (!validOtp) {
-			throw new NotFoundException('OTP code not found.');
+			throw new NotFoundException('Invalid or expired OTP.');
 		}
 		const deHashedOtp = await compare(otp, validOtp.otp_code);
 		if (validOtp && deHashedOtp) {
