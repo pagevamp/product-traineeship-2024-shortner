5a3bda68e2172cd8cca0036d5f0e0b75f688d74d
pratham-outside <pratham.sapkota@outside.tech>
2025-01-08 11:22:02 +0545

    fix: changed logger in user.service from scope based to class property

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,25 +9,25 @@ import { SuccessResponse } from '@/common/response.interface';
 import { successMessage } from '@/common/messages';
 @Injectable()
 export class UsersService {
+	private readonly logger = new Logger();
 	constructor(
 		@InjectRepository(User)
 		private userRepository: Repository<User>,
 	) {}
 	async create(createUserDto: CreateUserDto): Promise<SuccessResponse | undefined> {
-		const logger = new Logger();
 		try {
 			const passwordHash = await hash(createUserDto.password, env.SALT_ROUND);
 			const user = { ...createUserDto, password_hash: passwordHash };
 			const createdUser = (await this.userRepository.insert(user)).generatedMaps[0];
 			if (createdUser) {
-				logger.log(` New user ${createUserDto.name}  created`);
+				this.logger.log(` New user ${createUserDto.name}  created`);
 				return {
 					status: HttpStatus.CREATED,
 					message: successMessage.user_created,
 				};
 			}
 		} catch (error) {
-			logger.error(error);
+			this.logger.error(error);
 		}
 	}
 }
