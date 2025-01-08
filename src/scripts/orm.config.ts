5d8725960e4e947c0c021933fa801a8ad4492ea4
Narayan Sapkota <59175636+noxiousghost@users.noreply.github.com>
2025-01-08 12:19:04 +0545

    PDT24-49 | Fix migration issues (#18)
    * fix: make migration name dynamic and add migration revert script
    
    * refactor: rename typeorm scripts

--- src/scripts/orm.config.ts
+++
@@ -1,7 +0,0 @@
-import { writeFile } from 'fs/promises';
-import { DataSource } from 'typeorm';
-import { dataBaseConfigurations } from '@/database/db.config';
-
-const dataSource = new DataSource(dataBaseConfigurations);
-writeFile('ormconfig.json', JSON.stringify(dataSource.options, null, 2));
-export { dataSource };
