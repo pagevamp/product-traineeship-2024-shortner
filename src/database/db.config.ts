f571dc49f63f94ccbca6f2d4c0a07509d325b12a
Narayan Sapkota <sapkota.king@gmail.com>
2025-01-10 18:45:26 +0545

    chore: make changes specific to render deployment

--- src/database/db.config.ts
+++ src/database/db.config.ts
@@ -7,10 +7,17 @@ config();
 export const dataBaseConfigurations: DataSourceOptions = {
 	type: 'postgres',
 	port: +env.POSTGRES_PORT,
+	host: env.DB_HOST,
 	username: env.POSTGRES_USER,
 	password: env.POSTGRES_PASSWORD,
 	database: env.POSTGRES_DB,
 	synchronize: false,
+	ssl: true,
+	extra: {
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	},
 	logging: true,
 	entities: [join(__dirname, '/../**/entities', '*.entity.{ts,js}')],
 	migrations: [join(__dirname, '/../database/migrations', '*.{ts,js}')],
