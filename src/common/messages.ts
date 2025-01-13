106bdf9f2488a50fa5791024b4a5394e04858c94
pratham-outside <pratham.sapkota@outside.tech>
2025-01-13 15:42:09 +0545

    refactor: added and updated error messages

--- src/common/messages.ts
+++ src/common/messages.ts
@@ -5,11 +5,11 @@ export const successMessage = {
 
 export const errorMessage = {
 	userCreationFailure: 'User not created',
-  userNotFound: 'User not found',
+	userNotFound: 'User not found',
 	invalidCredentials: 'Invalid Credentials. Please check your credentials',
 	notVerified: 'User not verified. Please verify your account first',
-	tokenMissing: 'Invalid or expired token',
-  minLengthValidation: 'Must be at least 5 characters long',
+	tokenMissing: 'Authentication token missing. Please make sure you are logged in',
+	minLengthValidation: 'Must be at least 5 characters long',
 	maxLengthValidation: 'Must less than 15 characters',
-
+	authenticationFailed: 'Could not authenticate user. Has the session expired?',
 };
