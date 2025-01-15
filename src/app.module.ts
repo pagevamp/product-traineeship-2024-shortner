a7f05e10f56da3a00799fea91a746128725217d9
Narayan Sapkota <59175636+noxiousghost@users.noreply.github.com>
2025-01-15 16:22:57 +0545

    PDT24-51 | Configure Global Exception Filters (#20)
    * chore: configure http exception filter for consistent error handling
    
    * refactor: use relative imports and variable refactoring
    
    * fix: remove ! in stack key
    
    * refactor: convert http exception filter to all exceptions filter
    
    * chore: set retry after header if http status is of Too_Many_Requests
    
    * chore: add filters for displaying validation error messages as well

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { ConfigModule } from '@nestjs/config';
 import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { validate } from '@/config/env.config';
@@ -9,6 +10,7 @@ import { RateLimitMiddlewareFactory } from '@/middleware/reateLimit.middleware';
 import { urlRateLimiter } from '@/config/rateLimit.config';
 import { VerificationModule } from '@/verification/verification.module';
 import { UsersModule } from '@/users/users.module';
+import { AllExceptionsFilter } from '@/core/all-exceptions.filter';
 import { MailerModule } from '@/mailer/mailer.module';
 import { LoggerModule } from '@/logger/logger.module';
 @Module({
@@ -22,7 +24,13 @@ import { LoggerModule } from '@/logger/logger.module';
 		LoggerModule,
 	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [
+		AppService,
+		{
+			provide: APP_FILTER,
+			useClass: AllExceptionsFilter,
+		},
+	],
 })
 export class AppModule {
 	configure(consumer: MiddlewareConsumer): void {
