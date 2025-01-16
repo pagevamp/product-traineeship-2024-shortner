4e23d242479d9be954594ba44e1d0297bce2ba04
Narayan Sapkota <sapkota.king@gmail.com>
2025-01-16 23:02:15 +0545

    fix: fix expiaryDate validation

--- src/short-urls/dto/create-short-url.dto.ts
+++ src/short-urls/dto/create-short-url.dto.ts
@@ -1,5 +1,6 @@
 import { IsDate, IsOptional, IsUrl, MinDate, ValidateIf } from 'class-validator';
 import { errorMessage } from '@/common/messages';
+import { Transform } from 'class-transformer';
 
 export class CreateShortUrlDto {
 	@IsUrl()
@@ -8,6 +9,7 @@ export class CreateShortUrlDto {
 	@IsOptional()
 	@IsDate()
 	@ValidateIf((o) => o.expires_at !== null)
+	@Transform(({ value }) => new Date(value), { toClassOnly: true })
 	@MinDate(new Date(), { message: errorMessage.currentDateValidation })
 	expiryDate: Date;
 }
