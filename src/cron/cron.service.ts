235fa00443e29e2c0c83cec1376c2e150d15667e
pratham-outside <pratham.sapkota@outside.tech>
2025-01-21 11:59:42 +0545

    chore: changed cron to run everyday at midnight

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -5,7 +5,7 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 @Injectable()
 export class CronService {
 	constructor(private readonly shortUrlService: ShortUrlsService) {}
-	@Cron(CronExpression.EVERY_10_SECONDS)
+	@Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
 	async checkExpiry(): Promise<void> {
 		await this.shortUrlService.checkURLExpiry();
 	}
