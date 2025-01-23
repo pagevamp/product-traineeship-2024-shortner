296786e6e43807730dc1730fbbd133680266b436
Narayan Sapkota <sapkota.king@gmail.com>
2025-01-23 15:17:54 +0545

    chore: add missing columns, user_id and country in redirection_logs table

--- src/database/migrations/1737529897767-create_redirection_logs_table.ts
+++ src/database/migrations/1737529897767-create_redirection_logs_table.ts
@@ -6,16 +6,28 @@ export class CreateRedirectionLogsTable1737529897767 implements MigrationInterfa
 			CREATE TABLE "redirection_logs" (
             id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
             short_url_id uuid NOT NULL,
+            user_id uuid NOT NULL,
             clicked_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
             ip_address character varying(45),
+            country character varying(100),
             user_agent character varying,
             browser character varying(100),
             device character varying(100),
             CONSTRAINT FK_redirection_logs_shortened_urls FOREIGN KEY (short_url_id)
                 REFERENCES shortened_urls (id)
                 ON DELETE CASCADE
+                ON UPDATE CASCADE,
+            CONSTRAINT FK_redirection_logs_users FOREIGN KEY (user_id)
+                REFERENCES users (id)
+                ON DELETE CASCADE
                 ON UPDATE CASCADE
             )`);
+		await queryRunner.query(`
+            CREATE INDEX IX_redirection_logs_short_url_id ON redirection_logs(short_url_id)
+            `);
+		await queryRunner.query(`
+            CREATE INDEX IX_redirection_logs_clicked_at ON redirection_logs using brin (clicked_at)
+            `);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
--- src/url-analytics/entities/url-analytics.entity.ts
+++ src/url-analytics/entities/url-analytics.entity.ts
@@ -1,4 +1,5 @@
 import { ShortUrl } from '@/short-urls/entities/short-url.entity';
+import { User } from '@/users/entities/user.entity';
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('redirection_logs')
@@ -13,11 +14,21 @@ export class UrlAnalytics {
 	@JoinColumn({ name: 'short_url_id' })
 	short_url: ShortUrl;
 
+	@Column()
+	user_id: string;
+
+	@ManyToOne(() => User, { onDelete: 'CASCADE' })
+	@JoinColumn({ name: 'user_id' })
+	user: User;
+
 	@CreateDateColumn({ type: 'timestamptz' })
 	clicked_at: Date;
 
 	@Column({ type: 'varchar', length: 45, nullable: true })
 	ip_address: string;
+
+	@Column({ type: 'varchar', length: 100, nullable: true })
+	country: string;
 
 	@Column({ type: 'text', nullable: true })
 	user_agent: string;
