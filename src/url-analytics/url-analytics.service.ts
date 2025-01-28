a73fa5968c6392ba9823aa7b3c48ac69edd058d3
Narayan Sapkota <sapkota.king@gmail.com>
2025-01-28 12:46:01 +0545

    refactor: code cleanup

--- src/url-analytics/url-analytics.service.ts
+++ src/url-analytics/url-analytics.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UrlAnalytics } from '@/url-analytics/entities/url-analytics.entity';
 import { Repository } from 'typeorm';
-import { LoggerService } from '@/logger/logger.service';
 import { CreateUrlAnalyticsDto } from '@/url-analytics/dto/create-analytics.dto';
 import { lookup } from 'geoip-country';
 import { UAParser } from 'ua-parser-js';
@@ -12,49 +11,23 @@ export class UrlAnalyticsService {
 	constructor(
 		@InjectRepository(UrlAnalytics)
 		private analyticsRepo: Repository<UrlAnalytics>,
-		private readonly logger: LoggerService,
 	) {}
 
 	async createAnalytics(records: CreateUrlAnalyticsDto): Promise<void> {
-		try {
-			const { shortUrlId, userAgent, userId, ipAddress } = records;
-			const { browser, device, os } = await this.parseUserAgent(userAgent);
-			const country = await this.getCountryFromIP(ipAddress);
-			const analytics = {
-				short_url_id: shortUrlId,
-				user_id: userId,
-				user_agent: userAgent,
-				ip_address: ipAddress,
-				browser,
-				device,
-				operating_system: os,
-				country,
-			};
-			const redirectionLogs = await this.analyticsRepo.insert(analytics);
-			console.log(redirectionLogs);
-		} catch (error) {
-			console.log(error);
-			return;
-		}
-	}
-
-	async getUserSpecificAnalysis(userId: string): Promise<UrlAnalytics[]> {
-		const reports = await this.analyticsRepo.find({
-			where: {
-				user_id: userId,
-			},
-			select: {
-				id: true,
-				short_url_id: true,
-				clicked_at: true,
-				ip_address: true,
-				country: true,
-				browser: true,
-				device: true,
-				operating_system: true,
-			},
-		});
-		return reports;
+		const { shortUrlId, userAgent, userId, ipAddress } = records;
+		const { browser, device, os } = await this.parseUserAgent(userAgent);
+		const country = await this.getCountryFromIP(ipAddress);
+		const analytics = {
+			short_url_id: shortUrlId,
+			user_id: userId,
+			user_agent: userAgent,
+			ip_address: ipAddress,
+			browser,
+			device,
+			operating_system: os,
+			country,
+		};
+		await this.analyticsRepo.insert(analytics);
 	}
 
 	private async parseUserAgent(
@@ -72,7 +45,6 @@ export class UrlAnalyticsService {
 
 	private async getCountryFromIP(ip: string): Promise<string | undefined> {
 		const location = lookup(ip);
-		console.log(location);
 		return location?.country;
 	}
 }
